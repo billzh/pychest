0a8624c6fba135068487c9a901f6ce80a709e20a
billzh <zbill2000@gmail.com>
2020-05-19 21:36:17 -0400

    communicate on object that encrypt by pickle

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 import socket
 from _thread import *
-import sys
+from typing import List
+from player import Player
+import pickle
 
-server = "192.168.0.10"
+server = "192.168.0.16"
 port = 5555
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,36 +17,31 @@ except socket.error as e:
 s.listen(2)
 print("waiting for a connection, sever started")
 
-def read_pos(str):
-    str = str.split(",")
-    return int(str[0]), int(str[1])
-
-def make_pos(tup):
-    return str(tup[0]) + "," + str(tup[1])
-
-pos = [(0,0),(100,100)]
+player1 = Player(0,0,50,50,(255,0,0))
+player2 = Player(100,100,50,50,(0,0,255))
+players = [player1, player2]
 
 def thread_client(conn, player):
-    conn.send(str.encode(make_pos(pos[player])))
+    conn.send(pickle.dumps(players[player]))
     reply = ""
     while True:
         try:
-            data = read_pos(conn.recv(2048).decode())
-            pos[player] = data
+            data = pickle.loads(conn.recv(2048))
+            players[player] = data
 
             if not data:
                 print("disconected")
                 break
             else:
                 if player == 1:
-                    reply = pos[0]
+                    reply = players[0]
                 else:
-                    reply = pos[1]
+                    reply = players[1]
 
                 print("received: ", data)
                 print("sending: ", reply)
 
-            conn.sendall(str.encode(make_pos(reply)))
+            conn.sendall(pickle.dumps(reply))
         except:
             break
 
